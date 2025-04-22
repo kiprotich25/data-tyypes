aae4a1781aa85dc95fb2a648167e8d4308a33151
Kiprotich Ian <iankipro273@gmail.com>
2025-04-22 17:37:11 +0300

    Create data-types.py

---
+++ data-types.py
@@ -0,0 +1,15 @@
+print("Welcome to the data structures section 2!")
+my_list= []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1, 15)
+list_2=[50,60,70]
+my_list.extend(list_2)
+print(my_list)
+del my_list[-1]
+my_list.sort()
+print(my_list)
+position=my_list.index(30)
+print("The index of 30 is:", position)
